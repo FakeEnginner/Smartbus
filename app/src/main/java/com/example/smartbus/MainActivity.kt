4673bf0a0a149a3262b425513a39130c231883f4
Dakshendra Singh <pal.dakshendra80@gmail.com>
2023-02-04 11:19:09 +0530

    main file update
    bug:- page is not responsive for different screen size

--- app/src/main/java/com/example/smartbus/MainActivity.kt
+++ app/src/main/java/com/example/smartbus/MainActivity.kt
@@ -3,19 +3,23 @@ package com.example.smartbus
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var database: ContactDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-
-
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        supportActionBar?.hide()
+        setContentView(R.layout.mainpage)
 
     }
 }
