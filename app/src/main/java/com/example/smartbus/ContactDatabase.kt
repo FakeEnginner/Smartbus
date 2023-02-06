4c8b5e61173f5b45a57416ead7008d02de260b83
Dakshendra Singh <pal.dakshendra80@gmail.com>
2023-02-06 12:32:18 +0530

    ui implement fragment

--- app/src/main/java/com/example/smartbus/ContactDatabase.kt
+++
@@ -1,7 +0,0 @@
-package com.example.smartbus
-
-import androidx.room.Database
-
-class ContactDatabase {
-
-}
--- app/src/main/java/com/example/smartbus/MainActivity.kt
+++ app/src/main/java/com/example/smartbus/MainActivity.kt
@@ -7,19 +7,21 @@ import android.view.Window
 import android.view.WindowManager
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.smartbus.databinding.ActivityMainBinding
+import androidx.databinding.DataBindingUtil
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var database: ContactDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         supportActionBar?.hide()
-        setContentView(R.layout.mainpage)
+        @Suppress("UNUSED VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
 
     }
 }
